23b35fd34a5025075f151ba16042384f249edf6b
Fernando Alba <fernando.alba@live.com.ar>
2022-03-27 21:48:02 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['./**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          500: '#e6eff6',
+          900: '#a0a3aa',
+        },
+        secondary: '#2bb3b1',
+        accent: {
+          500: '#bfdf32',
+          700: '#a3cc2d',
+          900: '#657f1c',
+        },
+      },
+      fontFamily: {
+        karla: ['Karla', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
